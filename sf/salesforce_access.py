1eb83b49bd4fd0e8170797c1ed54dc0141ca3b92
rfagaraz <47521434+rfagaraz@users.noreply.github.com>
2022-09-22 13:18:32 -0300

    Removing saved password

--- sf/salesforce_access.py
+++ sf/salesforce_access.py
@@ -4,8 +4,8 @@ import base64
 from salesforce_api import Salesforce
 
 def session():
-    client = Salesforce(username= "renato.fagaraz@greenpeace.org",
-                        password= base64.b64decode("SG9tZXJAMzEyMjAzMjc=").decode("utf-8"),
-                        security_token= "L0turnKP1tOU2sx3Nl19XA40",
+    client = Salesforce(username= "",
+                        password= base64.b64decode("").decode("utf-8"),
+                        security_token= "",
                         is_sandbox=False)
     return client
